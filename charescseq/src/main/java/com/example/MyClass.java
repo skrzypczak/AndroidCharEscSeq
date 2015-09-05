2811d4e26bec7954a182590008a5842fbbb259c8
Bill Skrzypczak <bill@webbuildersconsulting.com>
2015-09-05 11:07:58 -0500

    Initial commit

---
+++ charescseq/src/main/java/com/example/MyClass.java
@@ -0,0 +1,9 @@
+package com.example;
+
+public class MyClass {
+
+    public static void main(String args[]) {
+        System.out.println("She said \"Hello!\" to me.");
+        System.out.println("So I gave her a '\u2740'");
+    }
+}
